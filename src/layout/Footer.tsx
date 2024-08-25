f7e4fa0fa615ea6d85981ad8251ac1a74ac68181
Marylucy <marylucyatienoomenda@gmail.com>
2024-08-25 16:25:30 +0300

    MoviePage.tsx

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mt-auto border-t border-white/10  px-4 py-8">
       <p className="text-center">
         &copy;
-        <a href="https://github.com/JohnMwendwa"> John Mwendwa </a>
+        <a href="https://github.com/Marylucy123450"> Marylucy Atieno </a>
         {new Date().getFullYear()}
       </p>
     </footer>
